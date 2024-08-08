83b628f8d3c5ac0f43a2e78eef5c1f6da720a7c2
zmertens <zach.mm035@gmail.com>
2024-08-08 08:13:35 -0700

    init commit

---
+++ src/main/java/com/zmertens/corners/CornersApplication.java
@@ -0,0 +1,13 @@
+package com.zmertens.corners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CornersApplication.class, args);
+	}
+
+}
